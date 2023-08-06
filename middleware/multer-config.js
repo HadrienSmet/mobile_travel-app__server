19eeb2ec6dfc683c72f01da96b06a28b23cd9d75
Hadrien <smethadrien.96@gmail.com>
2023-08-06 18:05:12 +0200

    Trying to connect the client and the back

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
-const { Storage } = require("@google-cloud/storage");
-const path = require("path");
+// const { Storage } = require("@google-cloud/storage");
+// const path = require("path");
 
 const MIME_TYPES = {
     "image/jpg": "jpg",
@@ -8,64 +8,86 @@ const MIME_TYPES = {
     "image/png": "png",
 };
 
-const googleCloud = new Storage({
-    keyFilename: path.join(
-        __dirname,
-        `../${process.env.GOOGLE_APPLICATION_CREDENTIALS}`
-    ),
-    projectId: process.env.GCS_ID,
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "images");
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(" ").join("_");
+        const extension = MIME_TYPES[file.mimetype];
+        const nameWithoutExtension = name.split(".")[0];
+        console.log(nameWithoutExtension + Date.now() + "." + extension);
+        console.log(
+            "multer-config l:17 fullNewName: " +
+                nameWithoutExtension +
+                Date.now() +
+                "." +
+                extension
+        );
+        callback(null, nameWithoutExtension + Date.now() + "." + extension);
+    },
 });
 
-const gcFiles = googleCloud.bucket(process.env.GCS_SPLIT_STRING);
+module.exports = multer({ storage }).any("file");
 
-const storage = multer.memoryStorage();
+// const googleCloud = new Storage({
+//     keyFilename: path.join(
+//         __dirname,
+//         `../${process.env.GOOGLE_APPLICATION_CREDENTIALS}`
+//     ),
+//     projectId: process.env.GCS_ID,
+// });
 
-const fileFilter = (req, file, cb) => {
-    if (MIME_TYPES[file.mimetype]) {
-        cb(null, true);
-    } else {
-        cb(new Error("Invalid file type."), false);
-    }
-};
+// const gcFiles = googleCloud.bucket(process.env.GCS_SPLIT_STRING);
 
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-});
+// const storage = multer.memoryStorage();
 
-const uploadMiddleware = upload.any("file");
+// const fileFilter = (req, file, cb) => {
+//     if (MIME_TYPES[file.mimetype]) {
+//         cb(null, true);
+//     } else {
+//         cb(new Error("Invalid file type."), false);
+//     }
+// };
 
-const uploadToGCS = (file, req) => {
-    const timestamp = Date.now();
-    const fileName = `${path.parse(file.originalname).name}-${timestamp}.${
-        MIME_TYPES[file.mimetype]
-    }`;
-    const blob = gcFiles.file(fileName);
-    const stream = blob.createWriteStream({
-        metadata: {
-            contentType: file.mimetype,
-        },
-    });
-    stream.on("error", (err) => {
-        console.error(`Error uploading file ${fileName}:`, err);
-    });
-    stream.on("finish", () => {
-        console.log(`File ${fileName} uploaded.`);
-        req.files.push({ filename: fileName });
-    });
-    stream.end(file.buffer);
-};
+// const upload = multer({
+//     storage: storage,
+//     fileFilter: fileFilter,
+// });
 
-module.exports = (req, res, next) => {
-    uploadMiddleware(req, res, (err) => {
-        if (err) {
-            return res.status(400).json({ error: err.message });
-        }
-        req.files.forEach((file) => {
-            uploadToGCS(file, req);
-        });
-        setTimeout(() => {
-            next();
-        }, 2000);
-    });
-};
+// const uploadMiddleware = upload.any("file");
+
+// const uploadToGCS = (file, req) => {
+//     const timestamp = Date.now();
+//     const fileName = `${path.parse(file.originalname).name}-${timestamp}.${
+//         MIME_TYPES[file.mimetype]
+//     }`;
+//     const blob = gcFiles.file(fileName);
+//     const stream = blob.createWriteStream({
+//         metadata: {
+//             contentType: file.mimetype,
+//         },
+//     });
+//     stream.on("error", (err) => {
+//         console.error(`Error uploading file ${fileName}:`, err);
+//     });
+//     stream.on("finish", () => {
+//         console.log(`File ${fileName} uploaded.`);
+//         req.files.push({ filename: fileName });
+//     });
+//     stream.end(file.buffer);
+// };
+
+// module.exports = (req, res, next) => {
+//     uploadMiddleware(req, res, (err) => {
+//         if (err) {
+//             return res.status(400).json({ error: err.message });
+//         }
+//         req.files.forEach((file) => {
+//             uploadToGCS(file, req);
+//         });
+//         setTimeout(() => {
+//             next();
+//         }, 2000);
+//     });
+// };
