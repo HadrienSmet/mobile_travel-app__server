f923bb1ee62a4fe477e35aa04d515bf260428dc9
Hadrien <smethadrien.96@gmail.com>
2023-08-12 20:24:22 +0200

    Adapted the the user data for the new app

--- models/User.js
+++ models/User.js
@@ -1,24 +1,44 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
+const positionSchema = mongoose.Schema({
+    long: { type: String, required: false },
+    lat: { type: String, required: false },
+});
+const durationSchema = mongoose.Schema({
+    arrival: { type: String, required: false },
+    departure: { type: String, required: false },
+});
+const pictureSchema = mongoose.Schema({
+    url: { type: String, required: false },
+    location: [positionSchema],
+});
 const albumSchema = mongoose.Schema({
     name: { type: String, required: false },
-    pictures: { type: [String], required: false },
+    pictures: [pictureSchema],
 });
 
+const tripTipsSchema = mongoose.Schema({
+    location: [positionSchema],
+    about: { type: String, required: false },
+    tips: { type: String, required: false },
+    picture: { type: String, required: false },
+});
 const previousTripSchema = mongoose.Schema({
-    destination: { type: String, required: false },
-    year: { type: Number, required: false },
-    duration: { type: String, required: false },
+    countries: { type: [String], required: false },
+    duration: { type: durationSchema, required: false },
+    kind: { type: String, required: false },
     withWho: { type: String, required: false },
-    details: { type: String, required: false },
+    tripTips: [tripTipsSchema],
+    resume: { type: String, required: false },
 });
 
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    onTravel: { type: Boolean, required: false, default: false },
+    travelerType: { type: String, required: false },
     profilePicture: { type: String, required: false },
-    coverPicture: { type: String, required: false },
     description: { type: String, required: false },
     dreamTrips: { type: [String], default: undefined, required: false },
     previousTrips: [previousTripSchema],
@@ -29,8 +49,7 @@ const userSchema = mongoose.Schema({
     gender: { type: String, required: true },
     country: { type: String, required: true },
     nationality: { type: String, required: true },
-    followers: { type: [String], default: [] },
-    following: { type: [String], default: [] },
+    friends: { type: [String], default: [] },
 });
 
 userSchema.plugin(uniqueValidator);
